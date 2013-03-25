7e3fabc77a9b85813f7b47df1bbe579e08e56c4a
Kyle Stewart <kylestew@gmail.com>
2013-03-24 20:27:13 -0700

    app code booted up, time for mockups

---
+++ Courtside/Courtside/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Courtside
+//
+//  Created by Kyle Stewart on 3/24/13.
+//  Copyright (c) 2013 inVnt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Courtside/Courtside/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Courtside
+//
+//  Created by Kyle Stewart on 3/24/13.
+//  Copyright (c) 2013 inVnt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
