fc91d4b293a3534b5d3f5b6b019957d974490c2e
Kyle Stewart <kylestew@gmail.com>
2013-04-10 15:06:17 -0700

    Project created

---
+++ Hustle/Hustle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hustle
+//
+//  Created by Kyle Stewart on 4/10/13.
+//  Copyright (c) 2013 Nautilus Software LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
