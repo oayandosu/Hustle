f00655c7bc995ea0ca8f1fddd9a0c335420a53e8
Kyle Stewart <kylestew@gmail.com>
2013-04-16 15:56:23 -0700

    Dataloading done for Popular feed

--- Hustle/Hustle/Shot.m
+++ Hustle/Hustle/Shot.m
@@ -3,4 +3,15 @@
 
 @implementation Shot
 
+- (id)initWithDictionary:(NSDictionary *)dictionary {
+    self = [super initWithDictionary:dictionary];
+    if (self) {
+        // aggregate data types - I'm falttening the Shot -> Player relationship
+        NSDictionary* player = [dictionary objectForKey:@"player"];
+        self.player_name = [player objectForKey:@"name"];
+        self.player_avatar_url = [player objectForKey:@"avatar_url"];
+    }
+    return self;
+}
+
 @end
